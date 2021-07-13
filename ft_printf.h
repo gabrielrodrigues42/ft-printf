d72b9b1e4cd219c2e04177299e5d2a9bb0867a58
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2021-07-13 15:23:43 -0300

    initial files

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/09 18:35:26 by gandrade          #+#    #+#             */
+/*   Updated: 2021/07/13 15:22:26 by gandrade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_printf(const char *);
+
+#endif
