e974430318ce4ff118b428d563ec05a3a2cdbe8c
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2021-07-22 00:46:03 -0300

    update

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 18:35:24 by gandrade          #+#    #+#             */
-/*   Updated: 2021/07/22 00:36:40 by gandrade         ###   ########.fr       */
+/*   Updated: 2021/07/22 00:45:32 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	ft_printf(const char *format, ...)
 	va_list	args;
 	int		done;
 
-	done = //somefunction(format, args);
 	va_start(args, format);
+	done = //somefunction(format, args);
 	va_end(args);
 	return (done);
 }
