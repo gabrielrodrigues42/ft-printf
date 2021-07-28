ead0e4ca3efccf8240148db2d215a91d79d35bb1
Gabriel Rodrigues <rodriguesgabriel960@gmail.com>
2021-07-28 17:19:48 -0300

    ft_htoa converter in ascii

--- libft/ft_htoa.c
+++ libft/ft_htoa.c
@@ -6,7 +6,7 @@
 /*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/26 21:34:12 by gandrade          #+#    #+#             */
-/*   Updated: 2021/07/27 12:30:06 by gandrade         ###   ########.fr       */
+/*   Updated: 2021/07/28 17:18:25 by gandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,10 @@ char	*ft_htoa(unsigned int n, int converter)
 	res = malloc(size + 1);
 	if (!res)
 		return (NULL);
+	if (converter == 'x')
+		converter = 87;
+	else
+		converter = 55;
 	ft_hexstr(res, n, size, converter);
 	return (res);
 }
