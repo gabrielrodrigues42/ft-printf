51dfd6e3856ed27229b5a1a3b5702559bd3553af
Gabriel Rodrigues <72752848+gabrielrodrigues42@users.noreply.github.com>
2021-11-11 13:09:53 -0300

    ft-printf update

--- libft/libft.h
+++ libft/libft.h
@@ -19,7 +19,6 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <limits.h>
 
 /*
 ** GNL Defines
@@ -95,7 +94,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-int		ft_strcmp(char *s1, char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
